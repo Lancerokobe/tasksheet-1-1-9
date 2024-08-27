b1cae6c20f6f1adc9043465acf8cb9db7f4c1325
Lancerokobe <141099012+Lancerokobe@users.noreply.github.com>
2024-08-27 12:22:48 +0000

    Creating first method

--- Tasksheet119.java
+++ Tasksheet119.java
@@ -1,1 +1,19 @@
+import java.util.*;
 
+public class Tasksheet119 {
+    public static double additions(double num1, double num2){
+        return num1+num2;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter 2 numbers: ");
+        double num1 = scan.nextDouble();
+        double num2 = scan.nextDouble();
+        
+        double addsum = additions(num1,num2);
+
+        System.out.println(" "+ addsum);
+        scan.close();
+    }
+}
